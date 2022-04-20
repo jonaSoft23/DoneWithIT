3d63ca6d43e93ccbaff13839a31290473f27208d
Owot jonathan <jonathanowot@gmail.com>
2022-04-20 19:40:35 +0300

    first commit

--- App.js
+++ App.js
@@ -2,9 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello world, it's ok</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +17,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    color: '#edd',
   },
 });
